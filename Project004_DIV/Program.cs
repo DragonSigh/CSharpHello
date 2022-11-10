d0ce41a89c2267ba7b40b299d698c220f9811561
DragonSigh <dragonsigh@gmail.com>
2022-11-10 20:04:15 +0300

    Added Random in Project 003

--- Project004_DIV/Program.cs
+++ Project004_DIV/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 90;
+double numberB = 11;
+Console.WriteLine(numberA / numberB);
