00e864d3f1d0d5dc62231827bc247e9223509a64
DragonSigh <dragonsigh@gmail.com>
2022-11-10 20:33:08 +0300

    added Project 006 old lesson

---
+++ Project006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 6;
+int c = 7;
+int d = 3;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.Write("max = ");
+Console.Write(max);
