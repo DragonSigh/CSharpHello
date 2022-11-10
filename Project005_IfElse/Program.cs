60582fb76af3e413cbd8645b8c98dae51e8332ef
DragonSigh <dragonsigh@gmail.com>
2022-11-10 20:26:53 +0300

    added Project 005 with if-else condition

---
+++ Project005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "вика")
+    {
+        Console.Write("Это сладкая вишнёвая булочка");
+    }
+else
+    {
+        Console.Write("Я тебя не знаю иди отсюда");
+    }
