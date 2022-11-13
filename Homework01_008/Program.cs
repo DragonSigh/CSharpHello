aa664f809854e91dfb52123576705fb72c4b516a
DragonSigh <dragonsigh@gmail.com>
2022-11-13 11:12:01 +0300

    added fourth homework

---
+++ Homework01_008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите целое число: ");
+int numberN = int.Parse(Console.ReadLine() ?? "");
+
+int i = 1;
+
+while (i <= numberN)
+ {
+    if (i % 2 == 0) Console.Write($"{i}, "); i++;
+ }
