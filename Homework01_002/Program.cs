f9214977fc300906d6453dc332bac16afef66273
DragonSigh <dragonsigh@gmail.com>
2022-11-13 10:55:38 +0300

    added first homework

---
+++ Homework01_002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое целое число: ");
+int numA = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе целое число: ");
+int numB = int.Parse(Console.ReadLine() ?? "");
+
+int maxNum = 0;
+
+if (numA > numB) maxNum = numA; else maxNum = numB;
+
+Console.WriteLine($"max = {maxNum}");
