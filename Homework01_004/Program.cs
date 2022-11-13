ec1c9e2750e55eb52bb59fd79097263c6a1266e9
DragonSigh <dragonsigh@gmail.com>
2022-11-13 11:00:35 +0300

    added second homework

---
+++ Homework01_004/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое целое число: ");
+int numA = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе целое число: ");
+int numB = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите третье целое число: ");
+int numC = int.Parse(Console.ReadLine() ?? "");
+
+int maxNum = numA;
+
+if (numB > maxNum) maxNum = numB;
+if (numC > maxNum) maxNum = numC;
+
+Console.WriteLine($"Максимальное из трёх чисел: {maxNum}");
