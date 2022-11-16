32bf8b9682549088321149482e5a0cf340952034
DragonSigh <dragonsigh@gmail.com>
2022-11-16 17:15:56 +0300

    added Homework task 010

---
+++ Homework02_010/Program.cs
@@ -0,0 +1,14 @@
+﻿int varA;
+
+while (true)
+{
+        Console.Write("Введите трёхзначное число: ");
+        if (int.TryParse(Console.ReadLine(), out varA))
+            if (varA > 99 && varA < 1000)
+              break;
+        Console.WriteLine("Ошибка ввода!");
+}
+
+int result = varA % 100 / 10;
+
+Console.WriteLine($"{varA} -> {result}");св
