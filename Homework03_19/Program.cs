0698f09abbe68201e90f0a2879891fe1a78f5f7a
Vladislav Kovnerov <x00ff@proton.me>
2022-11-21 16:23:10 +0300

    started Homework Seminar 03

---
+++ Homework03_19/Program.cs
@@ -0,0 +1,25 @@
+﻿int number1 = UserInputNumber();
+PrintPalindromeResult();
+
+// Метод для ввода пятизначного числа от пользователя
+static int UserInputNumber()
+{
+    int number = -1;
+    // Проверяем правильность ввода
+    while (true)
+    {
+        Console.Write("Введите пятизначное число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            if (number >= 10000 && number < 100000)
+                break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+
+// Метод для вывода результатов проверки палиндрома
+
+static void PrintPalindromeResult()
+{
+    
+}
