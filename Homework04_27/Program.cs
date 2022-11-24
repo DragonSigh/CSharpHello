d1d0796c53d1d556c3fea44fe0143625387bfca8
DragonSigh <dragonsigh@gmail.com>
2022-11-24 19:55:49 +0300

    added program for Homework 4 task 27

---
+++ Homework04_27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+var input_number = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+var result = GetSumOfDigitsInNumber(input_number);
+Console.WriteLine($"{input_number} -> {result}");
+
+// ---------------------------------------------------------------
+// Метод для получения суммы цифр в числе
+static int GetSumOfDigitsInNumber(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum = sum  + (n % 10);
+        n = n / 10;
+    }
+    return sum;
+}
+
+// ---------------------------------------------------------------
+// Метод для ввода целого числа от пользователя
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
