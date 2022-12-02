70990508421960e46832d3bcdc8d7d657c153309
Vladislav Kovnerov <x00ff@proton.me>
2022-12-02 20:09:34 +0300

    fix

--- Project013_Recursion/Program.cs
+++ Project013_Recursion/Program.cs
@@ -1,3 +1,9 @@
-﻿string[,] table = new string[2,5];
-table[1,2] = "слово";
-
+﻿double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
