05a8d341c34e4f71eb9c95dbce801f6216ec38f5
Vladislav Kovnerov <x00ff@proton.me>
2022-12-06 17:23:26 +0300

    added seminar 06 and seminar 07 tasks

---
+++ Seminar06_Task39/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] ReverseArray1(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArrau[inArray.Length - 1 - i ];
+    }
+    return result;
+}
+
+void ReverseArray2(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArrau[inArray.Length - 1 - i ];
+        inArray[inArray.Length - i - 1] = k;
+    }
+}
---
+++ Seminar07_Task51/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+// и замените эти элементы на их квадраты.
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine("->");
+Console.WriteLine(SquareEqualIndexes(array));
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int SquareEqualIndexes(int[,] inArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == j)
+                sum += inArray[i, j];
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == j) Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{inArray[i, j]} ");
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        Console.WriteLine();
+    }
+     Console.ResetColor();
+}
